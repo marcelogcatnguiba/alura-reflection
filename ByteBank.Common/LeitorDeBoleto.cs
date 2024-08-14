6c43b95645a384786c1af154ea2381d1f5a463b1
marcelo.gomes <marcelo.gomes@microdatasistemas.com.br>
2024-08-14 16:59:55 -0300

    Add leitura dinamica de propriedades

--- ByteBank.Common/LeitorDeBoleto.cs
+++ ByteBank.Common/LeitorDeBoleto.cs
@@ -1,57 +1,53 @@
-﻿namespace ByteBank.Common
+﻿using System.Reflection;
+
+namespace ByteBank.Common
 {
     public class LeitorDeBoleto
     {
         public List<Boleto> LerBoletos(string caminhoArquivo)
         {
-            //throw new NotImplementedException();
-
-            // montar lista de boletos
             var boletos = new List<Boleto>();
 
-            // ler arquivo de boletos
             using (var reader = new StreamReader(caminhoArquivo))
             {
-                // ler cabeçalho do arquivo CSV
                 string linha = reader.ReadLine()!;
                 string[] cabecalho = linha.Split(',');
 
-                // para cada linha do arquivo CSV
                 while (!reader.EndOfStream)
                 {
-                    // ler dados
                     linha = reader.ReadLine()!;
+                    
                     string[] dados = linha.Split(',');
+                    Boleto boleto = MapearTextoParaObjeto<Boleto>(cabecalho, dados);
 
-                    // carregar objeto Boleto
-                    Boleto boleto = MapearTextoParaBoleto(cabecalho, dados);
-
-                    // adicionar boleto à lista
                     boletos.Add(boleto);
                 }
             }
 
-            // retornar lista de boletos
             return boletos;
         }
 
-
-        private Boleto MapearTextoParaBoleto(string[] nomesPropriedades, string[] valoresPropriedades)
+        private static T MapearTextoParaObjeto<T>(string[] nomesPropriedades, string[] valoresPropriedades)
         {
-            Boleto instancia = new Boleto();
-            instancia.CedenteNome = valoresPropriedades[0];
-            instancia.CedenteCpfCnpj = valoresPropriedades[1];
-            instancia.CedenteAgencia = valoresPropriedades[2];
-            instancia.CedenteConta = valoresPropriedades[3];
-            instancia.SacadoNome = valoresPropriedades[4];
-            instancia.SacadoCpfCnpj = valoresPropriedades[5];
-            instancia.SacadoEndereco = valoresPropriedades[6];
-            instancia.Valor = Convert.ToDecimal(valoresPropriedades[7]);
-            instancia.DataVencimento = Convert.ToDateTime(valoresPropriedades[8]);
-            instancia.NumeroDocumento = valoresPropriedades[9];
-            instancia.NossoNumero = valoresPropriedades[10];
-            instancia.CodigoBarras = valoresPropriedades[11];
-            instancia.LinhaDigitavel = valoresPropriedades[12];
+            T instancia = Activator.CreateInstance<T>();
+            
+            for (int i = 0; i < nomesPropriedades.Length; i++)
+            {
+                var propriedade = nomesPropriedades[i];
+                PropertyInfo propertyInfo = instancia!.GetType().GetProperty(propriedade)!;
+
+                if(propertyInfo != null)
+                {
+                    Type propertyType = propertyInfo.PropertyType;
+
+                    string valorPropriedade = valoresPropriedades[i];
+
+                    object valorConvertido = Convert.ChangeType(valorPropriedade, propertyType);
+
+                    propertyInfo.SetValue(instancia, valorConvertido);
+                }
+            }
+            
             return instancia;
         }
     }
